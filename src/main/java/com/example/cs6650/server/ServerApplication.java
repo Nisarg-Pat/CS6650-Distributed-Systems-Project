20394eae923c6085c2573273e4907cdb8ec11e79
Nisarg Patel <patel321nisarg@gmail.com>
2022-12-04 17:49:57 -0500

    Initializing Spring App wih Maven

---
+++ src/main/java/com/example/cs6650/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs6650.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
